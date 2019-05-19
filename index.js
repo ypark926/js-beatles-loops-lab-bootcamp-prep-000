131f1b3317b3dd4e73ce971623ac763b6df78dd5
Yooni Park <yoonikj@gmail.com>
2019-05-19 23:19:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ function johnLennonFacts(facts) {
   var num = 0;
   var excArr = [];
   while (num < facts.length) {
-    excArr.push()
+    excArr.push(facts[num] + "!!!");
+    num++;
   }
+  return excArr;
+}
+
+function iLoveTheBeatles(num) {
+  var newArr = [];
+  do {
+    newArr.push("I love the Beatles!");
+    num++;
+  } while(num < 15);
+  
+  return newArr;
 }
