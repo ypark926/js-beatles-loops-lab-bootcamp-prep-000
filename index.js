71ae886845ce12cacb3a79b50c589fdccb976e9a
Yooni Park <yoonikj@gmail.com>
2019-05-19 22:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+function theBeatlesPlay(arrMusic, arrInst) {
+  var newArr = [];
+  
+  for (var i = 0; i < arrMusic.length; i++) {
+    newArr.push(arrMusic[i] + " plays " + arrInst[i]);
+  }
+  
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  var num = 0;
+  var excArr = [];
+  while (num < facts.length) {
+    excArr.push()
+  }
+}
